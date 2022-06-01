4f24bb110e35d0c029a06cfda119972f6d93e0c2
Hayden McCarthy <haydenmccarthy19@gmail.com>
2022-06-01 10:34:32 -0400

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+  'name': 'Google Doc Worksheet',
+  'version': '15.0',
+  'author': 'mccarthyhayden',
+  'category': 'Manufacturing',
+  'depends': ['mrp'],
+  'summary': 'Odoo module to allow Google Docs to be used as operations worksheets',
+}
